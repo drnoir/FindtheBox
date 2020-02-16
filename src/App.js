bb0eee9bde8c9c5c630edbce0ac6d2ddc1652eb5
Chris Godber <chrispgodber@gmail.com>
2020-02-16 20:51:04 +0300

    create box function prototype working
    random pos and create box working

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ class App extends React.Component {
       amount:0,
       boxes:[]
      };
-     this.createBoxes= this.createBoxes.bind(this);
-    //  this.createBoxe= this.createBox.bind(this);
+    //  this.createBoxes= this.createBoxes.bind(this);
+     this.createBox= this.createBox.bind(this);
   }
 
 
@@ -35,36 +35,19 @@ class App extends React.Component {
     })
   }
 
-  // createBox(){
-  //   function getRandomInt(max) {
-  //     return Math.floor(Math.random() * Math.floor(max));
-  //   }
-
-  //   let posx =getRandomInt(20);
-  //   let posz =getRandomInt(5);
-  //   let scale = getRandomInt(5)
-  //   console.log("create a box");
-  //   let newBox =<Entity geometry={{primitive: 'box'}} material={{color: 'green'}} scale={{x: scale, y: scale, z:scale}} position={{x: posx, y: 2, z: posz}}/> 
-  //  return  newBox 
-  // }
-
-
-  createBoxes(amount){
+  createBox(){
     function getRandomInt(max) {
       return Math.floor(Math.random() * Math.floor(max));
     }
 
-    let i;
-    for (i = 0; i < amount; i++) {
-      let posx =getRandomInt(20);
-      let posz =getRandomInt(5);
-      let scale = getRandomInt(5)
-      console.log("create a box");
-      let newBox =<Entity class={i} geometry={{primitive: 'box'}} material={{color: 'green'}} scale={{x: scale, y: scale, z:scale}} position={{x: posx, y: 2, z: posz}}/> 
-            
-    // console.log('for loop'+i+'and the amount it should loop'+this.state.amount)
-   return newBox
-  }}
+    let posx =getRandomInt(20);
+    let posz =getRandomInt(5);
+    let scale = getRandomInt(5)
+    console.log("create a box");
+    let newBox =<Entity geometry={{primitive: 'box'}} material={{color: 'green'}} scale={{x: scale, y: scale, z:scale}} position={{x: posx, y: 2, z: posz}}/> 
+
+    return newBox
+  }
 
   handleCollide = () => {
     alert('You found it!');
@@ -74,7 +57,8 @@ class App extends React.Component {
 
   render () {
   
-    let boxes = this.createBoxes(this.state.amount);
+    let box = this.createBox();
+  
     return (
     <Scene physics="debug: true">
       <Entity primitive='a-sky' color='black'/>
@@ -82,7 +66,8 @@ class App extends React.Component {
     <Entity particle-system={{preset: 'snow', particleCount: 3000}}/>
 
     <Entity static-body geometry={{primitive: 'box'}} material={{color: 'red'}}  scale={{x: this.state.scenex, y: 0, z:this.state.scenez}} position={{x: 0, y: 0, z: 0}}/>
-    {boxes}
+    {box}{box}{box}
+
     <Entity text={{value: 'Find the BlackBox'}}  scale={{x: 20, y: 20, z:20}} position={{x: 5, y:2, z: -5}}/>
     <h2>FIND THE PINK BOX</h2>
   </Scene>
